cdd71e70e3ece62cff82b82cbc3ba04b71e4c35c
Lowe Thiderman <daethorian@ninjaloot.se>
2011-03-14 20:49:22 +0100

    Grace commit

--- zsh/daethorian.zsh
+++ zsh/daethorian.zsh
@@ -58,7 +58,7 @@
         # using numerical arguments. If you give a boolean argument (true/false),
         # the prompt will set $PKEEP. If $PKEEP is true, the prompt will not
         # try to automatically switch to the CVS prompt.
-        export PMODE=2
+        export PMODE=4
         export PKEEP=false
 
         # Force the console to use prompt #1
