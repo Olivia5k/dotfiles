002cd791102342211a2c7dd999ff822c8d1d986c
Lowe Thiderman <daethorian@ninjaloot.se>
2011-03-24 20:08:04 +0100

    Add cool zsh cd function

--- zsh/modules/chpwd.zsh
+++ zsh/modules/chpwd.zsh
@@ -14,6 +14,34 @@ function chpwd()
     ls
 }
 
+
+function cd () {
+    local opt=""
+    if [[ ${+2} = 0 ]]; then
+        if [[ -f $1 ]]; then
+            builtin cd $opt $1:h
+        else
+            if ! builtin cd $opt $1 && [[ $#@ -eq 1 && ! -d $1 ]]; then
+                echo -En "cd: $1 doesn't exist, do you want to create it? [y/N] "
+                read -sq && mkcd $1
+            fi
+        fi
+    else
+        if [[ -z $3 ]]; then
+            builtin cd $opt "$1" "$2"
+        else
+            echo cd: too many arguments
+        fi
+    fi
+}
+
+function mkcd () {
+  test -z "$1" && echo >&2 "mkcd: no path given" && return
+  test -d "$1" && echo >&2 "mkcd: Directory $1 already exists"
+  mkdir -p -- "$1"
+  cd -- "$1"
+}
+
 # cdr <3
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
