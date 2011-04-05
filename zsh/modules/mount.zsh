639361bff466e10256d1981f1d23801eadcb1b5f
Lowe Thiderman <daethorian@ninjaloot.se>
2011-04-05 13:15:46 +0200

    zsh: disable host completion and refix mounting

--- zsh/modules/mount.zsh
+++ zsh/modules/mount.zsh
@@ -3,13 +3,11 @@ alias mmu='mount /mnt/usb'
 alias muu='umount /mnt/usb'
 
 # Warez
-alias mmw='sshfs ninjaloot.se:/warez /mnt/warez -o allow_other'
-alias mmwl='sshfs 10.0.0.6:/warez /mnt/warez -o allow_other'
+alias mmw='sshfs nl:/warez /mnt/warez -o allow_other'
 alias muw='fusermount -u /mnt/warez'
 
 # sshfs home
-alias mmn='sshfs ninjaloot.se: ~/ssh/ninjaloot'
-alias mmnl='sshfs 10.0.0.6: ~/ssh/ninjaloot'
+alias mmn='sshfs nl: ~/ssh/ninjaloot'
 alias mun='fusermount -u ~/ssh/ninjaloot'
 
 # Force unmounting
