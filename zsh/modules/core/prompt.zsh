1579cabbcabd6787673f9f7fbbbc3b6299568abd
Lowe Thiderman <lowe.thiderman@gmail.com>
2012-05-23 23:20:48 +0200

    Clean up prompt
    Clean up the mess left behind by the different prompt modes.
    Simplify the flow by removing the superfluous prompt() function.
    
    Adapt the git prompt to show the full path merged with the repo path.

--- zsh/modules/core/prompt.zsh
+++ zsh/modules/core/prompt.zsh
@@ -4,116 +4,61 @@ setopt prompt_subst
 setopt sunkeyboardhack
 export KEYBOARD_HACK="'"
 
-function p() {
-    if [[ "$1" = "3" ]] ; then
-        export PKEEP=false
-    else
-        export PKEEP=true
-    fi
+repo="%B%F{${c[14]}}%r%F{${c[1]}}" # Repo root
+repo+="(%F{${c[15]}}%u%b%F{${c[1]}})" # Branch and status
+repo+=":%F{${c[4]}}/%S" # Actual location
 
+zstyle ':vcs_info:*' formats "${repo}"
+
+function p() {
     export PMODE=$1
 }
 
 function precmd() {
-    if [[ $TERM != "linux" ]] ; then
+    if [[ $TERM != "linux" ]]; then
         # Print xterm title
         print -Pn "\e]0;%n@%m: %~\a"
-
-        # Add svn?
-        if [[ $PWD =~ "git/" ]] || [[ -d "$PWD/.git" ]] ; then
-            if ! $PKEEP && [[ $PMODE != 3 ]] ; then
-                export POLD=$PMODE
-                export PMODE=4
-            fi
-        fi
     fi
 
-    if [[ $PMODE -ge 3 ]] ; then
-        if [[ $PMODE = 4 ]]; then
-            repo="%B%F{${c[14]}}%r%F{${c[1]}}(%F{${c[15]}}%u%b%F{${c[1]}}):%F{${c[4]}}/%S"
-            branch=""
-        elif [[ $PMODE = 5 ]] ; then
-            repo="%B%F{${c[14]}}%r%F{${c[1]}}:%F{${c[4]}}/%S"
-            branch=" %F{${c[15]}}%u%b%%b"
-        else
-            repo="%B%F{${c[1]}}├─[%F{${c[13]}}%s%F{${c[1]}}:%F{${c[14]}}%r%F{${c[1]}}]"
-            branch="─[%F{${c[15]}}%u%b%F{${c[1]}}]%%b"
-        fi
-
-        zstyle ':vcs_info:*' formats "${repo}${branch}"
-        vcs_info
-
-        # If the message is empty, we are no longer in the CVS.
-        if [[ -z "${vcs_info_msg_0_}" ]] ; then
-            export PMODE=$POLD
-        fi
-    fi
-    prompt
-}
-
-function prompt() {
-    if [[ $PMODE -ge 2 ]] ; then
-        if [[ "$USER" != "$ALIAS" ]] ; then
-            local u="%n@%m"
-        else
-            local u="%m"
-        fi
-        if $HASMULTI && [[ $TERM != $MULTITERM ]] ; then
-            local dc=${c[5]}
-        else
-            local dc=${c[4]}
-        fi
-
-        if [[ -d $MAIL ]] ; then
-            local mc=$(find $MAIL | grep new/ | wc -l)
-            if [[ $mc -gt 0 ]] ; then
-                local m="─[%F{${c[9]}}${mc}M%F{${c[1]}}]"
-            fi
+    if [[ $PMODE -ge 2 ]]; then
+        local u="%m"
+        if [[ "$USER" != "$ALIAS" ]]; then
+            u="%n@%m"
         fi
 
         local t=""
-        if [[ -n "$TMOUT" ]] && [[ $TMOUT != 0 ]] ; then
-            local t="─[%F{${c[12]}}${TMOUT}%F{${c[1]}}]"
+        if [[ -n "$TMOUT" ]] && [[ "$TMOUT" != 0 ]]; then
+            local t="%F{${c[12]}}${TMOUT}s%F{${c[1]}}-" # Timeouting
         fi
 
-        local b=''
-        # Kernel battery info, with primitive caching! \o/
-        if false && $LAPTOP && [[ -f $BAT ]] ; then
-            if [[ -f $BATC ]] && [[ $(date +'%s') -le $BATT ]]; then
-                local bat=$(cat $BATC)
-            else
-                local bat=$(bat)
-                echo $bat > $BATC
+        local e="%(?..%F{${c[6]}}$(echo -en "\007")%?%F{${c[1]}}-)" # Errors
+        local j="%(1j.%F{${c[7]}}%jbg%F{${c[1]}}-.)" # Jobs
 
-                # 60 second cache
-                export BATT=$(($(date +'%s') + $BATS))
-            fi
-            local p=$(echo $bat | cut -f1 -d\|)
-            local t=$(echo $bat | cut -f2 -d\|)
+        local r1="%B%(#.%F{${c[3]}}%m.%F{${c[2]}}$u) %F{${c[4]}}"
 
-            local b="─[%F{${c[10]}}${p}%%%F{${c[1]}}]─[%F{${c[11]}}${t}%F{${c[1]}}]" # Battery
-        fi
+        vcs_info
+        if [[ -n "${vcs_info_msg_0_}" ]]; then
+            # Find the git root.
+            cur=$PWD
+            until [[ -z "$cur" ]]; do
+                if [[ -d "$cur/.git" ]]; then
+                    break
+                fi
+                cur=${cur%/*}
+            done
 
-        local e="%(?..─[%F{${c[6]}}$(bell)%?%F{${c[1]}}])" # Errorcodes
-        local j="%(1j.─[%F{${c[7]}}%j%F{${c[1]}}].)" # Jobs
-
-        if [[ $PMODE -ge 4 ]]; then
-            if [[ -n "${vcs_info_msg_0_}" ]]; then
-                local r1="%B%(#.%F{${c[3]}}%m.%F{${c[2]}}$u) ${vcs_info_msg_0_} "
-            else
-                local r1="%B%(#.%F{${c[3]}}%m.%F{${c[2]}}$u) %F{${c[4]}}%~ "
-            fi
-            local r2="%(?..%F{${c[6]}}$(bell)%?)%F{${c[1]}}%B>%b%f "
+            # Print in directory blue up until the repo.
+            r1+="${${cur/$HOME/\~}:h}/${vcs_info_msg_0_}"
         else
-            local r1="%B%F{${c[1]}}┌─[%(#.%F{${c[3]}}%m.%F{${c[2]}}$u)%F{${c[1]}}]$t─[%F{$dc}%~%F{${c[1]}}]%b"
-            local r2="%B%F{${c[1]}}└${e}${b}${j}${m}─[%F{${c[8]}}%D{%H:%M}%F{${c[1]}}]>%f%b "
+            r1+="%~"
         fi
+
+        local r2="${t}${j}${e}%F{${c[1]}}%B>%b%f "
     fi
 
     case $PMODE in
         0) ; PROMPT="%# "; ;;
         1) ; PROMPT="%B%(#.%F{${c[3]}}%m.%F{${c[2]}}%n@%m) %F{${c[4]}}%~ %#%b%f " ; ;;
-        2 | 4) ; PROMPT=$(print "$r1\n$r2") ; ;;
-        3) ; PROMPT=$(print "$r1\n${vcs_info_msg_0_}\n$r2") ; ;;
+        *) ; PROMPT=$(print "$r1\n$r2") ; ;;
     esac
 }
