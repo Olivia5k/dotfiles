d27f57a03ce6d2177147ef46f203abc7fed16bb5
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-01-15 16:41:15 +0100

    emacs: Remove ruby default stuffs *kreks*

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -29,8 +29,6 @@
 (show-paren-mode t)
 
 
-(add-to-list 'auto-mode-alist '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
 (add-to-list 'auto-mode-alist '("^/tmp/zshec" . sh-mode))
 
