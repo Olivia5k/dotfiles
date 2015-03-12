40a3e745582f6ba2c5c5b55999ddb9e8596127e6
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-12 23:15:34 +0100

    emacs: Add stolen default navigational binds

--- emacs.d/lisp/interface.el
+++ emacs.d/lisp/interface.el
@@ -46,4 +46,50 @@
 
 (add-hook 'emacs-lisp-mode-hook 'rainbow-identifiers-mode)
 
+
+
+(defun back-to-indentation-or-beginning ()
+  "Go to first non whitespace character on a line, or if already on the first
+  non whitespace character, go to the beginning of the line."
+  (interactive)
+  (if (= (point) (save-excursion (back-to-indentation) (point)))
+      (beginning-of-line)
+    (back-to-indentation)))
+
+(defun move-end-of-line-or-next-line ()
+  (interactive)
+  (if (eolp)
+      (next-line)
+    (move-end-of-line nil)))
+
+(defun insertline-and-move-to-line ()
+  "Insert an empty line after the current line and positon
+  the curson at its beginning, according to the current mode."
+  (interactive)
+  (move-end-of-line nil)
+  (open-line 1)
+  (forward-line 1)
+  (indent-according-to-mode))
+
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
+(global-set-key (kbd "M-g") 'goto-line)
+(global-set-key (kbd "C-a") 'back-to-indentation-or-beginning)
+(global-set-key (kbd "C-e") 'move-end-of-line-or-next-line)
+(global-set-key (kbd "C-o") 'insertline-and-move-to-line)
+
+(global-set-key (kbd "M-j")
+                (lambda ()
+                  (interactive)
+                  (join-line -1)))
+
 (provide 'interface)
