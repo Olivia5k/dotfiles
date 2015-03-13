ba333be0cefcb5e71299dd077d1e6df6857e2317
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-13 14:13:48 +0100

    emacs: Add linewise duplication, killing and yanking

--- emacs.d/lisp/interface.el
+++ emacs.d/lisp/interface.el
@@ -49,7 +49,42 @@
 
 (add-hook 'emacs-lisp-mode-hook 'rainbow-identifiers-mode)
 
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+   If there's no region, the current line will be duplicated. However, if
+   there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
 
+(global-set-key (kbd "C-d") 'duplicate-current-line-or-region)
+
+(global-set-key (kbd "M-k")
+                (lambda ()
+                  (interactive)
+                  (beginning-of-line)
+                  (kill-line 1)
+                  (back-to-indentation)))
+
+(defun yank-entire-line ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (set-mark-command)
+    (end-of-line)
+    (kill-ring-save)))
 
 (defun back-to-indentation-or-beginning ()
   "Go to first non whitespace character on a line, or if already on the first
