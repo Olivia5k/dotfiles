3676830819faf81d554dcf237b8562a150b3f303
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-17 12:16:35 +0100

    emacs: Update snakecharmer

--- emacs.d/lisp/snakecharmer.el
+++ emacs.d/lisp/snakecharmer.el
@@ -1,24 +1,14 @@
-(defun python-goto-or-add-docstring (&optional flags)
+(defun snake-goto-or-add-docstring (&optional flags)
   "Go to the docstring for the current class or function. Create it if it does
-not already exist."
- (interactive)
- (re-search-backward
-   "^\\([ ]*\\)\\(class\\|def\\) \\([a-zA-Z0-9_]+\\)" nil t)
-  (next-line)
-  (move-beginning-of-line 1)
+   not already exist."
+  (interactive)
+  (re-search-backward
+   "^\\( *\\)\\(class\\|def\\) \\([a-zA-Z0-9_]+\\)" nil t)
+  (forward-to-indentation 1)
   (if (looking-at "\"\"\"")
       (next-line)
     (progn
-
-      (newline-and-indent)
-      (previous-line))))
-      ;; (insert "\"\"\"")
-      ;; (newline-and-indent)
-      ;; (newline-and-indent)
-      ;; (insert "\"\"\"")
-      ;; (newline-and-indent)
-      ;; (newline-and-indent)
-      ;; (previous-line 3))))
+      (insert "\"\"\"\n%s\n\n\"\"\"\n\n"))))
 
 ;(define-key python-mode (kbd "C-c d") 'python-goto-or-add-docstring)
 
