06caf7ac3111d8802f9593d48f2225af58435917
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-20 23:23:55 +0100

    emacs: Make magit be beautiful fullscreen

--- emacs.d/lisp/magit.el
+++ emacs.d/lisp/magit.el
@@ -1,8 +1,17 @@
-(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "M-g") 'magit-status)
 
-(provide 'magit)
+(setq magit-save-some-buffers 'dontask
+      )
 
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
 
+(defadvice magit-mode-quit-window (after magit-restore-screen activate)
+  "Restores the previous window configuration and kills the magit buffer"
+  (jump-to-register :magit-fullscreen))
 
+(define-key magit-status-mode-map (kbd "q") 'magit-mode-quit-window)
 
-
+(provide 'local-magit)
