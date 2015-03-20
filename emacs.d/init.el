53e770ed8b8591aea0d99b8bf757e0104c0d51db
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-20 23:24:36 +0100

    emacs: Switch to zenburn

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -115,7 +115,7 @@
     (add-to-list 'custom-theme-load-path path)))
 
 (set-default-font "Inconsolata-16")
-(load-theme 'ujelly t)
+(load-theme 'zenburn t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
