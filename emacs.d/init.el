fb91b3fe2b59558f82a886c11ddfd7653717ae69
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-20 23:24:15 +0100

    emacs: Fix backup shenaniganz

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -68,6 +68,22 @@
         (find-alternate-file (concat "/su::" buffer-file-name)))))
 
 
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+        (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 2               ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 20            ; number of keystrokes between auto-saves (default: 300)
+      )
+
 ;; window switching
 (define-key global-map (kbd "C-<tab>")
   (lambda () (interactive) (select-window (next-window))))
