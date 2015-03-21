31b514070aaf0001db67cd1761d9b8dc0a3d6e92
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-03-21 01:21:29 +0100

    emacs: Clean up package setup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,10 +6,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+(package-initialize)
 
 (require 'package)
 (eval-after-load "package"
