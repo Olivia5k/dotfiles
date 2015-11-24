54d586f112170cd8511d46d404298725607d6cf8
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-11-24 21:53:02 +0100

    emacs: Require new library for development version

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,6 +4,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'url-handlers)
 (package-initialize)
 
 (require 'org)
