2b5a4dd2f72f2cdbf802d0b90ba839db2ff2f039
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-07-05 09:45:18 +0200

    emacs: Custom...

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -10,7 +10,7 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("aef93845026271eef513daa3700619d27168fb094d5e15db2eef81af52faea3c" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "38e99f77afd16890a501a47a918c88793e56775bcce47924c6732128cd80cd5d" "440c1bd2007fca5379c6b954a5362ce66bf6a7083a77c6ba860d3094a08a61ec" "dd7491df4e7df285f3869c253a93ae77659fc1394cb7938d76ddd41585120309" "7abf5a28ec511e7e8f5fe10978b3d63058bbd280ed2b8d513f9dd8b7f5fc9400" default)))
+    ("0ae09e79d0a3a7c9f31522fa325c7b9f248e5f5481e9b4a0c3bb9f3a91f221f1" "aef93845026271eef513daa3700619d27168fb094d5e15db2eef81af52faea3c" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "38e99f77afd16890a501a47a918c88793e56775bcce47924c6732128cd80cd5d" "440c1bd2007fca5379c6b954a5362ce66bf6a7083a77c6ba860d3094a08a61ec" "dd7491df4e7df285f3869c253a93ae77659fc1394cb7938d76ddd41585120309" "7abf5a28ec511e7e8f5fe10978b3d63058bbd280ed2b8d513f9dd8b7f5fc9400" default)))
  '(fci-rule-color "#373b41")
  '(gofmt-command "gofmt")
  '(httpd-port 8001)
@@ -18,9 +18,11 @@
  '(js2-mode-show-strict-warnings nil)
  '(js2-strict-missing-semi-warning nil)
  '(js2-strict-trailing-comma-warning nil)
+ '(org-journal-dir "~/org/journal/")
+ '(org-journal-file-format "%Y-%m-%d")
  '(package-selected-packages
    (quote
-    (web-mode vue-mode guide-key use-package color-theme-sanityinc-tomorrow restclient zenburn-theme zenburn yaml-mode xterm-color xkcd wrap-region visual-regexp virtualenvwrapper undo-tree twittering-mode twitter transpose-frame tidy string-inflection ssh-config-mode spaceline solarized-theme smex smart-mode-line-powerline-theme rainbow-mode rainbow-identifiers rainbow-delimiters python-django puppet-mode popwin pony-mode paredit paradox otp org-jekyll org-fstree org-bullets nginx-mode multiple-cursors move-text morlock markdown-mode magit-gh-pulls lispy key-chord js2-mode jedi ipython ioccur ido-vertical-mode ido-ubiquitous hydra httpcode htmlize helm-swoop helm-projectile helm-descbinds helm-dash helm-c-yasnippet helm-ag go-eldoc gitignore-mode gitconfig-mode git-timemachine git-messenger git-gutter-fringe gist flymake-css flycheck-pyflakes flx-ido fiplr fill-column-indicator expand-region emmet-mode dockerfile-mode django-mode discover-my-major diminish cyberpunk-theme csv-mode crontab-mode company-jedi company-go company-anaconda buffer-move browse-kill-ring adaptive-wrap ack-and-a-half ace-jump-mode ac-dabbrev)))
+    (leuven-theme org-journal web-mode vue-mode guide-key use-package color-theme-sanityinc-tomorrow restclient zenburn-theme zenburn yaml-mode xterm-color xkcd wrap-region visual-regexp virtualenvwrapper undo-tree twittering-mode twitter transpose-frame tidy string-inflection ssh-config-mode spaceline solarized-theme smex smart-mode-line-powerline-theme rainbow-mode rainbow-identifiers rainbow-delimiters python-django puppet-mode popwin pony-mode paredit paradox otp org-jekyll org-fstree org-bullets nginx-mode multiple-cursors move-text morlock markdown-mode magit-gh-pulls lispy key-chord js2-mode jedi ipython ioccur ido-vertical-mode ido-ubiquitous hydra httpcode htmlize helm-swoop helm-projectile helm-descbinds helm-dash helm-c-yasnippet helm-ag go-eldoc gitignore-mode gitconfig-mode git-timemachine git-messenger git-gutter-fringe gist flymake-css flycheck-pyflakes flx-ido fiplr fill-column-indicator expand-region emmet-mode dockerfile-mode django-mode discover-my-major diminish cyberpunk-theme csv-mode crontab-mode company-jedi company-go company-anaconda buffer-move browse-kill-ring adaptive-wrap ack-and-a-half ace-jump-mode ac-dabbrev)))
  '(paradox-github-token t)
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
@@ -60,6 +62,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(mmm-code-submode-face ((t nil)))
  '(mode-line ((t (:foreground "#000000" :background "#dddddd" :box nil))))
  '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
