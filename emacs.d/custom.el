cca5b46c3e3363585718e78c85681a35a46f2b77
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-08-06 14:26:41 +0200

    emacs: Web stuff

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -7,22 +7,26 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#373b41"))
+ '(css-indent-offset 2)
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("0ae09e79d0a3a7c9f31522fa325c7b9f248e5f5481e9b4a0c3bb9f3a91f221f1" "aef93845026271eef513daa3700619d27168fb094d5e15db2eef81af52faea3c" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "38e99f77afd16890a501a47a918c88793e56775bcce47924c6732128cd80cd5d" "440c1bd2007fca5379c6b954a5362ce66bf6a7083a77c6ba860d3094a08a61ec" "dd7491df4e7df285f3869c253a93ae77659fc1394cb7938d76ddd41585120309" "7abf5a28ec511e7e8f5fe10978b3d63058bbd280ed2b8d513f9dd8b7f5fc9400" default)))
+    ("3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "0ae09e79d0a3a7c9f31522fa325c7b9f248e5f5481e9b4a0c3bb9f3a91f221f1" "aef93845026271eef513daa3700619d27168fb094d5e15db2eef81af52faea3c" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "38e99f77afd16890a501a47a918c88793e56775bcce47924c6732128cd80cd5d" "440c1bd2007fca5379c6b954a5362ce66bf6a7083a77c6ba860d3094a08a61ec" "dd7491df4e7df285f3869c253a93ae77659fc1394cb7938d76ddd41585120309" "7abf5a28ec511e7e8f5fe10978b3d63058bbd280ed2b8d513f9dd8b7f5fc9400" default)))
  '(fci-rule-color "#373b41")
+ '(global-semantic-idle-scheduler-mode nil)
+ '(global-semanticdb-minor-mode nil)
  '(gofmt-command "gofmt")
  '(httpd-port 8001)
  '(js-indent-level 2)
  '(js2-mode-show-strict-warnings nil)
  '(js2-strict-missing-semi-warning nil)
  '(js2-strict-trailing-comma-warning nil)
+ '(linum-format " %7i ")
  '(org-journal-dir "~/org/journal/")
  '(org-journal-file-format "%Y-%m-%d")
  '(package-selected-packages
    (quote
-    (leuven-theme org-journal web-mode vue-mode guide-key use-package color-theme-sanityinc-tomorrow restclient zenburn-theme zenburn yaml-mode xterm-color xkcd wrap-region visual-regexp virtualenvwrapper undo-tree twittering-mode twitter transpose-frame tidy string-inflection ssh-config-mode spaceline solarized-theme smex smart-mode-line-powerline-theme rainbow-mode rainbow-identifiers rainbow-delimiters python-django puppet-mode popwin pony-mode paredit paradox otp org-jekyll org-fstree org-bullets nginx-mode multiple-cursors move-text morlock markdown-mode magit-gh-pulls lispy key-chord js2-mode jedi ipython ioccur ido-vertical-mode ido-ubiquitous hydra httpcode htmlize helm-swoop helm-projectile helm-descbinds helm-dash helm-c-yasnippet helm-ag go-eldoc gitignore-mode gitconfig-mode git-timemachine git-messenger git-gutter-fringe gist flymake-css flycheck-pyflakes flx-ido fiplr fill-column-indicator expand-region emmet-mode dockerfile-mode django-mode discover-my-major diminish cyberpunk-theme csv-mode crontab-mode company-jedi company-go company-anaconda buffer-move browse-kill-ring adaptive-wrap ack-and-a-half ace-jump-mode ac-dabbrev)))
+    (smooth-scrolling worf curl-for-url leuven-theme org-journal web-mode vue-mode guide-key use-package color-theme-sanityinc-tomorrow restclient zenburn-theme zenburn yaml-mode xterm-color xkcd wrap-region visual-regexp virtualenvwrapper undo-tree twittering-mode twitter transpose-frame tidy string-inflection ssh-config-mode spaceline solarized-theme smex smart-mode-line-powerline-theme rainbow-mode rainbow-identifiers rainbow-delimiters python-django puppet-mode popwin pony-mode paredit paradox otp org-jekyll org-fstree org-bullets nginx-mode multiple-cursors move-text morlock markdown-mode magit-gh-pulls lispy key-chord js2-mode jedi ipython ioccur ido-vertical-mode ido-ubiquitous hydra httpcode htmlize helm-swoop helm-projectile helm-descbinds helm-dash helm-c-yasnippet helm-ag go-eldoc gitignore-mode gitconfig-mode git-timemachine git-messenger git-gutter-fringe gist flymake-css flycheck-pyflakes flx-ido fiplr fill-column-indicator expand-region emmet-mode dockerfile-mode django-mode discover-my-major diminish cyberpunk-theme csv-mode crontab-mode company-jedi company-go company-anaconda buffer-move browse-kill-ring adaptive-wrap ack-and-a-half ace-jump-mode ac-dabbrev)))
  '(paradox-github-token t)
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
@@ -64,6 +68,7 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:background nil))))
  '(mmm-code-submode-face ((t nil)))
+ '(mmm-default-submode-face ((t nil)))
  '(mode-line ((t (:foreground "#000000" :background "#dddddd" :box nil))))
  '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
  '(mode-line-inactive ((t (:foreground "#caaaca" :background "#eeeeee" :box nil))))
@@ -71,4 +76,7 @@
  '(org-block-background ((t (:background "#000000"))))
  '(org-block-begin-line ((t (:foreground "#008ED1" :background "#002E41"))))
  '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
+ '(web-mode-html-attr-equal-face ((t (:foreground "gray70"))))
+ '(web-mode-html-attr-name-face ((t (:foreground "cyan"))))
+ '(web-mode-html-tag-face ((t (:foreground "PaleGreen"))))
  '(which-func ((t (:foreground "#77aaff")))))
