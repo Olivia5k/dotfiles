a9959fcf0c00fb7605816a5aa4b6ff56ac69888c
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-11-15 14:27:32 +0100

    emacs: Fix background bug

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -94,6 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(mmm-code-submode-face ((t nil)))
  '(mmm-default-submode-face ((t nil)))
  '(web-mode-html-attr-equal-face ((t (:foreground "gray70"))))
