1bbbba902506fa85e81b8ccdf14e39cd2a858706
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-11-25 16:31:24 +0100

    fish: Set theme to gruvbox

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,5 @@ set -x PATH $PATH $HOME/git/dotfiles/util
 set -x WORK_EMAIL thiderman@spotify.com
 set -x WORK_REMOTE ghe
 set -x PAYMENTS_SRC $HOME/spotify/payments/
+
+set -g theme_color_scheme gruvbox
