26b1ec43902e6bf9b335021b2930627431ad9c3c
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-01-18 15:59:32 +0100

    emacs: Put the first things first

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,11 @@
 ; Nothing is in here. Check out ./emacs.org for the actual configuration.
 
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
+(when (fboundp 'mouse-wheel-mode) (mouse-wheel-mode 1))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
