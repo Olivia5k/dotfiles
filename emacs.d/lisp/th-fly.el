b629f4662a1631b41ef742b9120dcd3c8884d4d3
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-06-26 18:38:57 +0200

    emacs: Bring back C-; as iedit

--- emacs.d/lisp/th-fly.el
+++ emacs.d/lisp/th-fly.el
@@ -4,13 +4,16 @@
   ("C-x C-p" . flycheck-previous-error)
 
   :config
-  (setq flycheck-checkers
-   '(css-csslint emacs-lisp go-gofmt go-golint go-vet go-build go-test html-tidy json-jsonlint json-python-json less python-flake8 python-pylint python-pycompile sass scss-lint scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck sql-sqlint yaml-jsyaml yaml-ruby))
+  (setq flycheck-checkers '(css-csslint emacs-lisp go-gofmt go-golint go-vet go-build go-test html-tidy json-jsonlint json-python-json less python-flake8 python-pylint python-pycompile sass scss-lint scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck sql-sqlint yaml-jsyaml yaml-ruby))
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
+(use-package flyspell
+  :bind
+  ("C-;" . iedit-mode)
+  :config
+  (add-hook 'prog-mode-hook (lambda () (flyspell-prog-mode)))
+  (add-hook 'text-mode-hook (lambda () (flyspell-mode))))
 
-;; Spell checking with flyspell
-(add-hook 'prog-mode-hook (lambda () (flyspell-prog-mode)))
-(add-hook 'text-mode-hook (lambda () (flyspell-mode)))
+
 
 (provide 'th-fly)
