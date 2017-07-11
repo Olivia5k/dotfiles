428d8fa7b18beb34df7d529cd38d6094ebb6dede
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-07-11 12:52:30 +0200

    emacs: Improve vue-mode with 5000%

--- emacs.d/lisp/th-web.el
+++ emacs.d/lisp/th-web.el
@@ -1,10 +1,126 @@
 (use-package vue-mode
+  :init
+  (require 'js)
+  (require 'css-mode)
+
+  ;; Interestingly enough, the bindings that are vue specific needs to
+  ;; be in all the maps.
+  ;; TODO(thiderman): Improve on this.
+  :bind (("C-c v" . th/vue-switcher)
+         :map vue-mode-map
+         ("C-c f" . th/vue-goto)
+         ("C-c s" . th/vue-toggle-style-scope)
+         ("C-c C-n" . th/vue-next-section)
+         ("C-c C-p" . th/vue-prev-section)
+         :map vue-html-mode-map
+         ("C-c f" . th/vue-goto)
+         ("C-c C-n" . th/vue-next-section)
+         ("C-c C-p" . th/vue-prev-section)
+         :map js-mode-map
+         ("C-c f" . th/vue-goto)
+         ("C-c C-n" . th/vue-next-section)
+         ("C-c C-p" . th/vue-prev-section)
+         :map css-mode-map
+         ("C-c f" . th/vue-goto)
+         ("C-c C-n" . th/vue-next-section)
+         ("C-c C-p" . th/vue-prev-section)
+         ("C-c s" . th/vue-toggle-style-scope))
+
   :mode "\\.vue\\'"
   :config
   (setq css-indent-offset 2)
-  (setq mmm-submode-decoration-level 0))
+  (setq mmm-submode-decoration-level 0)
+  (add-hook 'vue-mode-hook 'th/vue-auto-template-insert))
+
+(defun th/vue-switcher ()
+  (interactive)
+  (th/other-files-suffix "vue"))
+
+(defun th/vue-goto (kind)
+  "Go to the vue section specified by `kind'.
+
+Insert it if it does not exist."
+  (interactive
+   (list
+    (completing-read "vue section: " '(template script style) nil t)))
+
+  (beginning-of-buffer)
+  (if (ignore-errors (re-search-forward (format "^<%s" kind)))
+      ;; Found the section - go to the beginning of the line
+      (beginning-of-line)
+    ;; The section was not found. Add it.
+    (end-of-buffer)
+    (newline (if (and (bolp) (eolp)) 1 2))
+    (insert kind)
+    (when (string= kind "style")
+      (insert "[lang=scss]"))
+    (emmet-expand-line nil)
+    (newline 1 t)
+    (when (string= kind "style")
+      (th/vue-toggle-style-scope))))
+
+(defun th/vue-toggle-style-scope ()
+  "Add or remove the 'scoped' keyword to the <style> tag."
+  (interactive)
+  (save-excursion
+    (th/vue-goto "style")
+    (forward-word 1)
+    (when (looking-at " lang")
+      (forward-word 2)
+      (forward-char 1))
+    (if (looking-at " *>")
+        (insert " scoped")
+      (kill-word 1))))
+
+(defun th/vue-auto-template-insert ()
+  "Insert the template block when `vue-mode' is enabled in an empty buffer."
+  (when (= (buffer-size) 0)
+    (dolist (tag '("template" "div"))
+      (insert tag)
+      (emmet-expand-line nil)
+      (newline-and-indent))))
+
+(defun th/vue-goto-section (search-func)
+  "Search backwards or forwards for the beginning of a vue section"
+
+  (let ((success (ignore-errors (funcall search-func "^<[^/]"))))
+    (beginning-of-line)
+    success))
+
+(defun th/vue-prev-section ()
+  "Go to the previous vue section."
+  (interactive)
+  (th/vue-goto-section 're-search-backward))
+
+(defun th/vue-next-section ()
+  "Go to the next vue section."
+  (interactive)
+  (when (looking-at "^<")
+    ;; If we're already at the beginning of a tag, we need to move
+    ;; forward to be able to search past it.
+    (forward-char 1))
+
+  (when (not (th/vue-goto-section 're-search-forward))
+    ;; If it didn't work, check if that is because the next tag doesn't
+    ;; exist yet. If such is the case then create it.
+    (let ((l (length (th/vue-tags-in-buffer))))
+      (cond
+       ((= l 0)
+        (th/vue-goto "template"))
+       ((= l 1)
+        (th/vue-goto "script"))
+       ((= l 2)
+        (th/vue-goto "style"))))))
+
+(defun th/vue-tags-in-buffer ()
+  "Returns the current root tags in the buffer"
+
+  (-filter
+   (lambda (s) (and (s-prefix? "<" s) (not (s-prefix? "</" s))))
+   (s-split "\n" (buffer-substring-no-properties (point-min) (point-max)))))
 
 (use-package web-mode
+  :after vue-mode
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-attr-indent-offset 2)
@@ -12,26 +128,31 @@
 
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   ;; If we're in vue-mode, make sure to reset it when we save.
-  (add-hook 'after-save-hook
-            (lambda ()
-              (interactive)
-              (when (eq 'vue-mode major-mode)
-                (vue-mode)))))
+  (add-hook 'after-save-hook #'th/auto-html-vue))
 
-(defun th/vue-switcher ()
+(defun th/auto-html-vue ()
   (interactive)
-  (th/other-files-suffix "vue"))
+  (when (eq 'vue-mode major-mode)
+    (vue-mode)))
 
-(global-set-key (kbd "C-c v") #'th/vue-switcher)
 
 (use-package emmet-mode
+  :after vue-mode
+  :bind (:map emmet-mode-keymap
+              ("C-j" . th/emmet-vue-expand))
   :init
   (setq emmet-indentation 2)
 
   :config
-  (add-hook 'sgml-mode-hook 'emmet-mode)  ;; Auto-start on any markup modes
-  (add-hook 'css-mode-hook  'emmet-mode)  ;; Enable Emmet's css abbreviation.
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'css-mode-hook  'emmet-mode) ;; Enable Emmet's css abbreviation.
   (add-hook 'web-mode-hook  'emmet-mode))
+
+(defun th/emmet-vue-expand ()
+  "Use `emmet-expand-line' and set `emmet-use-css-transform' depending on the major mode."
+  (interactive)
+  (let ((emmet-use-css-transform (derived-mode-p 'css-mode)))
+    (emmet-expand-line nil)))
 
 (setq httpd-port 8001)
 
