73dc6deab2c4cc0aee711589f4f1987a3eeba275
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-07-28 01:53:12 +0200

    emacs: Add ANSI colorization of the compile buffer

--- emacs.d/lisp/th-compile.el
+++ emacs.d/lisp/th-compile.el
@@ -2,6 +2,13 @@
   :init
   (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
 
+  (ignore-errors
+    (require 'ansi-color)
+    (defun colorize-compilation-buffer ()
+      (when (eq major-mode 'compilation-mode)
+        (ansi-color-apply-on-region compilation-filter-start (point-max))))
+    (add-hook 'compilation-filter-hook 'colorize-compilation-buffer))
+
   (setq compilation-always-kill t)
   (setq compilation-ask-about-save nil)
   (setq compilation-auto-jump-to-first-error nil)
