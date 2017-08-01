3937eadb338aa3c810cdcfc35b165a8dcf0534ae
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-08-01 12:22:30 +0200

    emacs: Change scroll-step back to default

--- emacs.d/lisp/th-settings.el
+++ emacs.d/lisp/th-settings.el
@@ -4,7 +4,7 @@
 (setq browse-url-generic-program "chrome-app")
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message ";; *scratch*\n\n")
-(setq scroll-step 10)
+(setq scroll-step 0)
 (setq debug-on-error nil)
 (setq gc-cons-threshold 50000000)
 
