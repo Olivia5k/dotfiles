1238776365fff3d2580099eb6e746bb5b2416352
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-08-29 14:15:48 +0200

    Add docker-compose key

--- emacs/.emacs.d/lisp/th-docker.el
+++ emacs/.emacs.d/lisp/th-docker.el
@@ -2,7 +2,14 @@
   :bind (("C-x C-d" . docker-containers)
          (:map docker-containers-mode-map
                ("i" . docker-images))))
-(use-package dockerfile-mode)
+(use-package dockerfile-mode
+  :bind (:map dockerfile-mode-map
+              ("C-c C-c" . th/docker-compose)))
 (use-package docker-tramp)
+
+(defun th/docker-compose ()
+  "Run docker compose on the current file"
+  (interactive)
+  (compile "docker-compose build"))
 
 (provide 'th-docker)
