b0be916f92be3adc30ca15c00b0c0ade40b1dd6d
Lowe Thiderman <lowe.thiderman@gmail.com>
2017-10-04 16:00:35 +0200

    Update polybar colors

--- polybar/.config/polybar/stumpwmgroups.sh
+++ polybar/.config/polybar/stumpwmgroups.sh
@@ -6,6 +6,6 @@ echo "
                                                            ((string-equal name \"SLACK\") \"   SLACK  \")
                                                            (t (concat \"   \" name \"  \")))))
                                   (if (eq g (current-group))
-                                      (concat \"%{F#fff B#dda47dd0 u#3C3246 +u}\" display-text \"%{F- B- u- -u}\")
+                                      (concat \"%{F#fff B#dda47dd0 u#fff +u}\" display-text \"%{F- B- u- -u}\")
                                       (concat \"%{F#aaa}\" display-text \"%{F-}\"))))
                               (sort (screen-groups (current-screen)) #'< :key #'group-number)))" | stumpish -e eval
