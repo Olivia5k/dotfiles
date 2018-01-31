1181ddcbff8899b195e9cc924c1ed6cd7f395cd1
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-01-31 14:20:13 +0100

    Diminish flycheck

--- emacs/.emacs.d/lisp/th-last.el
+++ emacs/.emacs.d/lisp/th-last.el
@@ -3,6 +3,7 @@
   (diminish 'paredit-mode)
   (diminish 'auto-revert-mode)
   (diminish 'eldoc-mode)
+  (diminish 'flycheck-mode)
   (diminish 'auto-fill-function " fill")
   (diminish 'compilation-in-progress " 🔥"))
 
