e0d51a1dc45ff255b3eb3e1905c1dd8ab41883d1
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-04-25 12:54:31 +0200

    Add special list of docker-compose commands

--- emacs/.emacs.d/lisp/th-docker.el
+++ emacs/.emacs.d/lisp/th-docker.el
@@ -13,7 +13,33 @@
               ("C-c C-c" . docker-compose-execute-command)
               ("C-c C-b" . docker-compose-build-buffer)
               ("C-c C-s" . docker-compose-start-buffer))
-  :load-path "/home/thiderman/src/github.com/meqif/docker-compose-mode")
+  :load-path
+  "/home/thiderman/src/github.com/meqif/docker-compose-mode"
+  :init
+  (setq docker-compose-commands
+        (list
+         "build"
+         "build --no-cache"
+         "create"
+         "down"
+         "exec"
+         "images"
+         "logs --follow"
+         "logs --follow --tail=100"
+         "logs"
+         "ps"
+         "pull"
+         "push"
+         "restart"
+         "rm"
+         "rm -f"
+         "run"
+         "start"
+         "stop"
+         "top"
+         "up"
+         "up --build"
+         )))
 
 (defun th/docker-compose ()
   "Run docker compose on the current file"
