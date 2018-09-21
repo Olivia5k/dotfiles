6cfa292b8b7fe5c9bf2927674a8f3164ac08aad9
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-09-21 14:47:40 +0200

    Disable removed keymap

--- emacs/.emacs.d/lisp/th-docker.el
+++ emacs/.emacs.d/lisp/th-docker.el
@@ -1,7 +1,8 @@
 (use-package docker
   :bind (("C-x C-d" . docker-containers)
-         (:map docker-containers-command-map
-               ("i" . docker-images))))
+         ;; (:map docker-containers-command-map
+         ;;       ("i" . docker-images))
+         ))
 (use-package dockerfile-mode
   :bind (:map dockerfile-mode-map
               ("C-c C-c" . th/docker-compose)))
