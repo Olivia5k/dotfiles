4a7d3a94952f130df1bba7c36405d82a3b7528cf
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-12-03 00:19:48 +0100

    Why didn't I do this years ago

--- emacs/.emacs.d/lisp/th-web.el
+++ emacs/.emacs.d/lisp/th-web.el
@@ -144,6 +144,11 @@ Insert it if it does not exist."
 
 (use-package web-mode
   :after vue-mode
+  :bind
+  ("M-a" . web-mode-element-beginning)
+  ("M-e" . web-mode-element-end)
+  ("M-h" . web-mode-mark-and-expand)
+
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-attr-indent-offset 2)
