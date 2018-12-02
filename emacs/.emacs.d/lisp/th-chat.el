1b7403b40f024f83bf75ef245fff8c4430003cbf
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-12-02 17:09:56 +0100

    Disable unused chat code

--- emacs/.emacs.d/lisp/th-chat.el
+++ emacs/.emacs.d/lisp/th-chat.el
@@ -4,80 +4,10 @@
   :config
   (global-emojify-mode 1))
 
-(defun insert-image-from-url (&optional url)
-  (interactive)
-  (save-excursion
-    (unless url
-      (search-backward "http")
-      (setq url (url-get-url-at-point)))
-    (unless url
-      (error "Couldn't find URL.")))
-
-  (let ((buffer (url-retrieve-synchronously url)))
-    (unwind-protect
-         (let ((data (with-current-buffer buffer
-                       (goto-char (point-min))
-                       (search-forward "\n\n")
-                       (buffer-substring (point) (point-max)))))
-           (split-window-below)
-           (windmove-down)
-           (switch-to-buffer "*image*")
-           (local-set-key (kbd "q") 'th/kill-window)
-           (erase-buffer)
-           (insert-image (create-image data nil t)))
-      (kill-buffer buffer))))
-
-
-
-(defun th/circe-prompt ()
-  (lui-set-prompt
-   (concat (propertize (concat (buffer-name) ">")
-                       'face 'circe-prompt-face)
-           " ")))
-
-(use-package circe
-  :bind
-  (:map circe-query-mode-map
-        ("M-m" . emojify-insert-emoji))
-  (:map circe-channel-mode-map
-        ("M-m" . emojify-insert-emoji))
-  :config
-  (circe-set-display-handler "JOIN" (lambda (&rest ignored) nil))
-  (circe-set-display-handler "PART" (lambda (&rest ignored) nil))
-  (circe-set-display-handler "QUIT" (lambda (&rest ignored) nil))
-
-  (load "lui-logging" nil t)
-  (setq lui-logging-directory "~/.logs/circe/")
-  (make-directory lui-logging-directory t)
-  (enable-lui-logging-globally)
-
-  (add-hook 'circe-chat-mode-hook 'th/circe-prompt))
-
-
-(use-package slack
-  :commands (slack-start)
-  :init
-  (setq slack-buffer-emojify t) ;; if you want to enable emoji, default nil
-  (setq slack-prefer-current-team t)
-
-  (setq tracking-ignored-buffers
-        '("#gitlab"
-          "#notifications"
-          "#support"
-          "#infrastructure")))
-
-;; omfg how can the buffer names be so long by default
-(defmethod slack-room-buffer-name ((room slack-channel))
-  (concat "#" (slack-room-display-name room)))
-
-(defmethod slack-room-buffer-name ((room slack-group))
-  (concat (slack-room-display-name room)))
-
 (use-package alert
   :commands (alert)
   :init
   (setq alert-default-style 'libnotify))
-
 
 (defhydra th/chat (:exit t)
   "chat"
@@ -90,7 +20,6 @@
 
 (global-set-key (kbd "s-e") 'th/chat/body)
 
-
 (defadvice tracking-next-buffer (before tracking-small-windows activate)
   "Make chat buffers appear in smaller splits"
   (when (not (memq major-mode '(circe-query-mode
@@ -101,8 +30,7 @@
     (windmove-down)))
 
 ;; Load secret credentials and start the chats
-
-(add-to-list 'load-path "/home/thiderman/src/github.com/thiderman/secrets/emacs/.emacs.d/lisp")
-(load "th-chat-secret" t t)
+;; (add-to-list 'load-path "/home/thiderman/src/github.com/thiderman/secrets/emacs/.emacs.d/lisp")
+;; (load "th-chat-secret" t t)
 
 (provide 'th-chat)
